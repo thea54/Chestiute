c9e2264786342ae194a1ea18e9ab62d9e7de71f7
Thea Anca <teea_anca@yahoo.com>
2018-10-27 20:44:06 +0300

    added lecturer

---
+++ Lab4/src/main/java/com/models/Lecturer.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.models;
+
+/**
+ *
+ * @author Asus
+ */
+public class Lecturer {
+    private String name;
+    private String course;
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the course
+     */
+    public String getCourse() {
+        return course;
+    }
+
+    /**
+     * @param course the course to set
+     */
+    public void setCourse(String course) {
+        this.course = course;
+    }
+    
+}
